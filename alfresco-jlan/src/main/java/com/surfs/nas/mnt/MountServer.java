f2189e8980515d3c79d06e49edda1e248d2e166c
surcloud <surcloud@sursen.net>
2016-03-26 22:33:05 +0800

    Added files via upload

--- alfresco-jlan/src/main/java/com/surfs/nas/mnt/MountServer.java
+++ alfresco-jlan/src/main/java/com/surfs/nas/mnt/MountServer.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2016 SurCloud.
+ *
+ * This file is part of JLAN for SurFS
+ *
+ * JLAN for SurFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JLAN for SurFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JLAN for SurFS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.surfs.nas.mnt;
 
 import org.alfresco.jlan.app.JLANServer;
